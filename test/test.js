663a78f116bd3285fa046c62011e3c78769a6c1d
ednagc <ednagc.11@gmail.com>
2017-04-26 18:55:58 +0100

    Corrigiendo pruebas

--- test/test.js
+++ test/test.js
@@ -1,16 +1,25 @@
 'use strict'
-var Shape = require('../lib/triangle.js');
-var Triangle = require('@ednagc/ull-shape-triangle-ednagc');
+var Triangle = require('../lib/triangle.js');
+var Shape = require('@ull-edna-joseluis-kevin-35l2/ull-shape');
 
 describe("getArea", function() {
+
+  it("Creación objeto Shape", function(){
+    let sh = new Triangle ({width: 100, height: 100});
+    sh.should.not.undefined();
+  })
+
   it("Calculo del area de un triangulo correctamente. Objeto tipo Shape", function() {
-    let a = new Shape({ width: 100, height: 50 }, 'Triangle');
-    let s = a.getArea();
-    s.should.match(/^5000$/);
+    let a = new Shape({ width: 100, height: 100 }, 'Triangle');
+    let result = a.getArea();
+    result.should.equal(5000);
   })
+
   it("Calculo del area de un triangulo correctamente. Objeto Triangle", function() {
-    let a = new Triangle({ width: 100, height: 50 });
-    let s = a.getArea();
-    s.should.match(/^5000$/);
+    let a = new Triangle({ width: 100, height: 100 });
+    let result = a.getArea();
+    result.should.equal(5000);
   })
+
+
 });
